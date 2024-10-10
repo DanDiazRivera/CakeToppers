076f2caf6a20e01ab41e95f8a3ab27aadd30f13c
CBowneUCF <156275809+CBowneUCF@users.noreply.github.com>
2024-10-09 20:45:34 -0400

    Extra Fruit in order

--- Assets/Scripts/Systems/OrderManager.cs
+++ Assets/Scripts/Systems/OrderManager.cs
@@ -35,44 +35,10 @@ public class OrderManager : Singleton<OrderManager>
 
 	public void SubmitDesert(D_Cake input)
 	{
-		int score = CompareCakes(input, currentOrder);
+		int score = currentOrder.Compare(input);
 		LevelManager.Get().AddScore(score);
 		Destroy(currentOrder.gameObject);
 		RandomizeDesert();
 	}
 
-	private int CompareCakes(D_Cake playerCake, D_Cake orderCake)
-	{
-		if (playerCake.cakeBase is null) return 0;
-		int finalScore = 0;
-
-		if (Ingredient.Compare(playerCake.cakeBase, orderCake.cakeBase)) finalScore += 50;
-		else finalScore += 5;
-
-		if (playerCake.frostingCover is null) finalScore += 0;
-		else if (Ingredient.Compare(playerCake.frostingCover, orderCake.frostingCover)) finalScore += 50;
-		else finalScore += 10;
-
-		if (playerCake.fruits.Count < 1) finalScore += 0;
-		else
-		{
-			finalScore += 10;
-
-			Vector2 inputStrawbPos = new (playerCake.fruits[0].transform.localPosition.x, playerCake.fruits[0].transform.localPosition.z);
-			Vector2 orderStrawbPos = new(orderCake.fruits[0].transform.localPosition.x, orderCake.fruits[0].transform.localPosition.z);
-
-			float distance = Vector2.Distance(inputStrawbPos, orderStrawbPos);
-
-			finalScore += distance switch
-			{
-				< 0.2192031f => 50,
-				< 0.4384062f => 25,
-				_ => 0
-			};
-
-			finalScore += playerCake.fruits.Count - 1;
-		}
-			
-		return finalScore;
-	}
 }

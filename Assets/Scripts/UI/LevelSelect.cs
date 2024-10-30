423bffe36dbb4e25145d28559ee667ca0dfa78e7
CBowneUCF <156275809+CBowneUCF@users.noreply.github.com>
2024-10-30 16:36:28 -0400

    Level Select Setup and stuff

--- Assets/Scripts/UI/LevelSelect.cs
+++ Assets/Scripts/UI/LevelSelect.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -9,42 +7,30 @@ public class LevelSelect : MonoBehaviour
     public GameObject WorldSelectPanel;
     public GameObject WorldPanel;
     public string nextScene;
-    
-    public void OpenPanel()
-    {
-        WorldPanel.SetActive(true);
-    }
-    public void ClosePanel()
-    {
-        WorldSelectPanel.SetActive(false);
-    }
-    
-    public void SceneSelect()
-    {
-        SceneManager.LoadScene(nextScene);
-    }
 
-    public void Back()
-    {
-        WorldPanel.SetActive(false);
-        WorldSelectPanel.SetActive(true);
-    }
+    public void SceneSelect() => SceneManager.LoadScene(nextScene);
 
     public void BeginLevel(LevelData levelData)
     {
         GameMainManager.BeginGame(levelData);
     }
 
-    public void ReturnToTitle()
-    {
-        SceneManager.LoadScene("MainMenu");
-    }
+    public void ReturnToTitle() => SceneManager.LoadScene("MainMenu");
 
     public Button[] levelButtons;
 
+    public RectTransform[] worlds;
+
     private void Awake()
     {
-        for (int i = 1; i < levelButtons.Length; i++) 
+        GameMainManager.Get();
+
+        for (int i = 1; i < levelButtons.Length; i++)
             levelButtons[i].interactable = GameMainManager.Get().AllLevels[i - 1].MinComplete();
+    }
+
+    public void ShowWorld(RectTransform thisWorld)
+    {
+        foreach (var world in worlds) world.gameObject.SetActive(world == thisWorld);
     }
 }

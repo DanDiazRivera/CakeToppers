eca87fddda02543fd1a357a961e18689eb7668b4
slapstickers <maxgeiringer@knights.ucf.edu>
2024-11-01 17:16:38 -0400

    Audio Mixer + Sliders

---
+++ Assets/Scripts/Audio/SoundFXManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class SoundFXManager : MonoBehaviour
+{
+    public static SoundFXManager instance;
+
+    [SerializeField] private AudioSource soundFXObject;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+    public void PlaySoundFXClip(AudioClip clip, Vector3 position, float volume)
+    {
+        AudioSource audioSource = Instantiate(soundFXObject, position, Quaternion.identity);
+        audioSource.clip = clip;
+        audioSource.volume = volume;
+        audioSource.Play();
+        float clipLength = audioSource.clip.length;
+        Destroy(audioSource.gameObject, clipLength);
+    }
+}

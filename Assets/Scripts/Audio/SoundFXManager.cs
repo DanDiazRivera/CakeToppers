d017ba82366d258d726a35e6689a9c0adc7a99fa
slapstickers <maxgeiringer@knights.ucf.edu>
2024-11-08 19:55:56 -0500

    Ambient Sounds Added

--- Assets/Scripts/Audio/SoundFXManager.cs
+++ Assets/Scripts/Audio/SoundFXManager.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.UIElements;
 
 public class SoundFXManager : MonoBehaviour
 {
     public static SoundFXManager instance;
 
     [SerializeField] private AudioSource soundFXObject;
+    [SerializeField] private AudioClip doorNoise;
+    [SerializeField] private Vector3 soundPos;
 
     private void Awake()
     {
@@ -21,6 +24,21 @@ public class SoundFXManager : MonoBehaviour
         AudioSource audioSource = Instantiate(soundFXObject, position, Quaternion.identity);
         audioSource.clip = clip;
         audioSource.volume = volume;
+        audioSource.Play();
+        float clipLength = audioSource.clip.length;
+        Destroy(audioSource.gameObject, clipLength);
+    }
+
+    public void PlayDoorNoise()
+    {
+        StartCoroutine(doorNoiseWait());
+    }
+
+    IEnumerator doorNoiseWait()
+    {
+        yield return new WaitForSeconds(2);
+        AudioSource audioSource = Instantiate(soundFXObject, soundPos, Quaternion.identity);
+        audioSource.clip = doorNoise;
         audioSource.Play();
         float clipLength = audioSource.clip.length;
         Destroy(audioSource.gameObject, clipLength);

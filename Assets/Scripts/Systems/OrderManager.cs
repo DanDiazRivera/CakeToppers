d85c37369cd79090eb481dc9402a0c4e072120ce
CBowneUCF <156275809+CBowneUCF@users.noreply.github.com>
2024-11-13 20:44:21 -0500

    Comparison Revamp

--- Assets/Scripts/Systems/OrderManager.cs
+++ Assets/Scripts/Systems/OrderManager.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 
 public class OrderManager : Singleton<OrderManager>
@@ -76,16 +78,143 @@ public class OrderManager : Singleton<OrderManager>
 
     public void SubmitDesert(D_Cake input)
     {
-        int score = Compare(currentOrder, input);
-        SoundFXManager.Get().PlaySound(compareData.Sound(score));
-        LevelManager.Get().AddScore(score);
+        LevelManager.Get().AddScore(Compare(currentOrder, input));
         Destroy(currentOrder.gameObject);
         RandomizeDesert();
     }
 
 
-    [SerializeField] private ComparisonData compareData = new();
+    [SerializeField] private ComparisonDataNew comp = new();
     [System.Serializable]
+    private class ComparisonDataNew
+    {
+        public int noCakeNegative = -20;
+        public int cakeValue = 50;
+        public int frostingValue = 50;
+        public int totalFruitValue = 50;
+
+        public float minIcingSimilarity = .3f;
+        public float maxIcingSimilarity = .8f;
+        public int maxIcingValue = 50;
+
+
+        public float minRatioForSound = .6f;
+
+        public SoundFXManager.RandomizedAudio goodSound;
+        public SoundFXManager.RandomizedAudio badSound;
+
+    }
+
+
+    public int Compare(D_Cake orderCake, D_Cake playerCake)
+    {
+        if (playerCake.cakeBase is null) return comp.noCakeNegative;
+        int finalScore = 0;
+
+        {
+            if (Ingredient.Compare(playerCake.cakeBase, orderCake.cakeBase)) finalScore += comp.cakeValue;
+            else finalScore += comp.cakeValue / 2;
+        } // Cake Base
+
+        {
+            if (Ingredient.Compare(playerCake.frostingCover, orderCake.frostingCover)) finalScore += comp.frostingValue;
+            else finalScore += comp.frostingValue / 2;
+        } // Frosting Cover
+
+        {
+            int orderFruitCount = orderCake.fruits.Count;
+            int playerFruitCount = orderCake.fruits.Count;
+
+            float indiFruitValue = ((float)comp.totalFruitValue) / orderFruitCount;
+            float fruitValueComp = 0;
+
+            float range = 1.3f;
+
+            var overlapResults = new RaycastHit[5];
+            for (int i = 0; i < orderFruitCount; i++)
+            {
+                int count = Physics.SphereCastNonAlloc(
+                    playerCake.transform.position + orderCake.fruits[i].transform.localPosition - Vector3.up,
+                    range, Vector3.up, overlapResults, 2, ~0);
+                Ingredient chosenFruit = null;
+                for (int i2 = 0; i2 < count; i2++)
+                    if (overlapResults[i2].transform.TryGetComponent(out Ing_Fruit thisFruit) && playerCake.fruits.Contains(thisFruit))
+                    {
+                        chosenFruit = thisFruit;
+                        break;
+                    }
+                if (chosenFruit != null)
+                {
+                    bool isClose = Vector2.Distance(chosenFruit.transform.localPosition, orderCake.fruits[i].transform.localPosition) < range / 3f;
+                    bool isCorrectType = Ingredient.Compare(chosenFruit, orderCake.fruits[i]);
+
+                    if (isClose && isCorrectType) fruitValueComp += indiFruitValue;
+                    else if (!isClose && isCorrectType) fruitValueComp += indiFruitValue / 2;
+                    else if (isClose && !isCorrectType) fruitValueComp += indiFruitValue / 3;
+                    else if (!isClose && !isCorrectType) fruitValueComp += indiFruitValue / 4;
+                }
+                else if (i <= playerFruitCount) fruitValueComp += indiFruitValue / 5;
+            }
+            if (playerFruitCount > orderFruitCount) fruitValueComp -= playerFruitCount - orderFruitCount * indiFruitValue / 2;
+            finalScore += (int)fruitValueComp;
+
+        } // Fruit
+
+        {
+            // Note, currently only 1 flavor of icing will generate, hence the assumption of 1.
+
+            Ing_FrostingDraw orderIcing = orderCake.icings[0];
+
+            Ing_FrostingDraw playerIcing = playerCake.icings.FirstOrDefault(check => Ingredient.Compare(check, orderIcing));
+            playerIcing ??= playerCake.icings[0];
+
+            if (playerIcing) finalScore += CompareIcing(orderIcing, playerIcing);
+
+        } // Icing
+
+        int possibleScore = 
+            comp.cakeValue + 
+            comp.frostingValue + 
+            (orderCake.fruits.Count > 0 ? comp.totalFruitValue : 0) + 
+            (orderCake.icings.Count > 0 ? comp.maxIcingValue : 0);
+
+        float ratio = (float)finalScore / (float)possibleScore;
+            
+        SoundFXManager.Get().PlaySound(ratio >= comp.minRatioForSound ? comp.goodSound : comp.badSound);
+
+        return finalScore;
+    }
+
+    private int CompareIcing(Ing_FrostingDraw orderIcing, Ing_FrostingDraw playerIcing)
+    {
+        float[] orderValues = orderIcing.texture.GetPixels(1).Select(color => color.r).ToArray();
+        float[] playerValues = playerIcing.texture.GetPixels(1).Select(color => color.r).ToArray();
+
+        float similarity = 0;
+        float total = 0;
+
+        for (int i = 0; i < orderValues.Length; i++)
+        {
+            //similarity += (1 - Mathf.Abs(orderValues[i] - playerValues[i])) * orderValues[i] * 1.5f * 1.5f;
+            similarity += playerValues[i] * orderValues[i] * 1.5f;
+            similarity -= playerValues[i] * (1 - orderValues[i]) * 0.5f;
+            total += orderValues[i];
+        }
+
+        similarity /= total;
+        similarity = (similarity - comp.minIcingSimilarity) / (comp.maxIcingSimilarity - comp.minIcingSimilarity);
+        similarity = Mathf.Clamp(similarity, 0, 1);
+
+        return (int)(similarity * comp.maxIcingValue * (Ingredient.Compare(playerIcing, orderIcing) ? 1 : 0.5f));
+    }
+
+
+
+    /*
+    #region Old Compare
+
+        [SerializeField, Obsolete] private ComparisonData compareData = new();
+    [System.Serializable, Obsolete]
     private struct ComparisonData
     {
         public int noCakeNegative;
@@ -109,7 +238,7 @@ public class OrderManager : Singleton<OrderManager>
         public AudioClip Sound(int score) => score > minValueForSound ? goodSound : badSound;
     }
 
-    public int Compare(D_Cake orderCake, D_Cake playerCake)
+    public int CompareOld(D_Cake orderCake, D_Cake playerCake)
     {
         if (playerCake.cakeBase is null) return compareData.noCakeNegative;
         int finalScore = 0;
@@ -160,7 +289,7 @@ public class OrderManager : Singleton<OrderManager>
             float similarity = 0;
             float total = 0;
 
-            /*
+            
             Color[] diffColors = new Color[orderValues.Length];
 
             for (int i = 0; i < orderValues.Length; i++)
@@ -173,7 +302,7 @@ public class OrderManager : Singleton<OrderManager>
 
             diffTex = new Texture2D(16, 16);
             diffTex.SetPixels(diffColors); diffTex.Apply();
-            */
+            
 
 
             for (int i = 0; i < orderValues.Length; i++)
@@ -194,5 +323,5 @@ public class OrderManager : Singleton<OrderManager>
 
         return finalScore;
     }
-
+    #endregion */
 }
